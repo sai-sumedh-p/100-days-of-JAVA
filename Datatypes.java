b01a36c5a10aba3c993f70b9c4ec8329ac59a3df
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-06-27 07:51:55 +0530

    data types

---
+++ Datatypes.java
@@ -0,0 +1,19 @@
+/*Data Type 	Description
+    byte	    Stores whole numbers from -128 to 127
+    short	    Stores whole numbers from -32,768 to 32,767
+    int	        Stores whole numbers from -2,147,483,648 to 2,147,483,647
+    long	    Stores whole numbers from -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807
+    float	    Stores fractional numbers. Sufficient for storing 6 to 7 decimal digits
+    double	    Stores fractional numbers. Sufficient for storing 15 to 16 decimal digits
+    boolean	    Stores true or false values
+    char	    Stores a single character/letter or ASCII values */
+
+public class Datatypes {
+    public static void main(String[] args){
+        int myNum = 5;               // Integer (whole number)
+float myFloatNum = 5.99f;    // Floating point number
+char myLetter = 'D';         // Character
+boolean myBool = true;       // Boolean
+String myText = "Hello";     // String
+    }
+}
