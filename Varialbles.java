553a285c03991b3bbbb3184e9349738245284e62
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-06-27 07:13:02 +0530

    java variables"

---
+++ Varialbles.java
@@ -0,0 +1,22 @@
+public class Varialbles {
+    public static void main(String[] args){
+        /*Variables are containers for storing data values.
+
+In Java, there are different types of variables, for example:
+
+String - stores text, such as "Hello". String values are surrounded by double quotes
+int - stores integers (whole numbers), without decimals, such as 123 or -123
+float - stores floating point numbers, with decimals, such as 19.99 or -19.99
+char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
+boolean - stores values with two states: true or false
+ */
+        String name="sumedh";
+        int age = 20;
+        double CGPA= 9.14 ;
+        
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(CGPA);
+    }
+}
