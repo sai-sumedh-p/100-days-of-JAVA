9d87d22bbd8139386a33df9637d8045f84534d70
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-06-27 06:58:27 +0530

    initial files

---
+++ Comment.java
@@ -0,0 +1,5 @@
+public class Comment{
+    public static void main(String[] args){
+        System.out.print("Hello world");//this is a print state ment whereSystem is a built in class,out stands for output
+    }
+}
