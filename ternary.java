939b20cbe1e576b468cd0c928e9b6ddbb6ba506f
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-06-28 13:14:34 +0530

    code

--- ternary.java
+++ ternary.java
@@ -1,6 +1,8 @@
 public class ternary {
     public static void main(String[] args){
-        String result=(8>9)?"8 is greater than 9":"9 is greater than 8";
+        int a = 8;
+        int b =9;
+        String result=(a>b)?"8 is greater than 9":"9 is greater than 8";
         System.out.println(result);
     }
     
