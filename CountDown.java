1e4b8f378667eec30ed95939a7f585f17788c292
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-06-28 13:44:52 +0530

    code

---
+++ CountDown.java
@@ -0,0 +1,10 @@
+public class CountDown {
+    public static void main(String[] args){
+        int countdown=5;
+        while(countdown>0){
+            System.out.println(countdown);
+            countdown--;
+        }
+        System.out.println("happy new year");
+    }
+}
