1c91898b9ee89713a66b84e58bf9ad88635f1a87
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-06-28 07:19:51 +0530

    code

--- Strings.java
+++ Strings.java
@@ -1,12 +1,27 @@
 import java.util.Scanner;
 public class Strings{
     public  static void main(String[] args){
+            String firstname;
+            String lastname;
             Scanner scanner=new Scanner(System.in);
             String name;
-            System.out.println("Enter your name");
-            name = scanner.nextLine();
-            scanner.close();
+            System.out.println("Enter your first name");
+            firstname = scanner.nextLine();
+             System.out.println("Enter your last name");
+            lastname = scanner.nextLine()+" ";
+            name=firstname.concat( lastname);
+            System.out.println("Your name is: " + name.toUpperCase() ); //makes the name to upper case letters
+            System.out.println("Your name is: " + name.toLowerCase() ); //makes the name to lower case letters
             System.out.println("The length of your name is: "+name.length()+" characters");
+            System.out.println("Your name is: " + name.toUpperCase() ); //makes the name to upper case letters
+            System.out.println("Your name is: " + name.toLowerCase() ); //makes the name to lower case letters
+            System.out.println("what do you want to find in your name");
+            String txt=scanner.nextLine();
+            System.out.println(txt+" starts at "+name.indexOf(txt)+" character");
 
+
+
+
+            scanner.close();
     }
 }
