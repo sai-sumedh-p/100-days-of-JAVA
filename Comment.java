e4b44d3db9d83c995fb264ee8e4e14bbdee7445b
sai-sumedh-p <saisumedhpedarla@gmail.com>
2025-06-29 22:27:33 +0530

    code

--- Comment.java
+++ Comment.java
@@ -1,5 +1,5 @@
 public class Comment{
     public static void main(String[] args){
-        System.out.print("Hello World");//this is a print statement whereSystem is a builtin class,out stands for output
+        System.out.print("Hello World welcome to java");//this is a print statement whereSystem is a builtin class,out stands for output
     }
 }
